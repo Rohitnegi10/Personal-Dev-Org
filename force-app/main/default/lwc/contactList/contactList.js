f83592ab4b8f051b8ae74475d36ff51de41aa9e9
rohitnegi10 <rohitnegicap10@gmail.com>
2024-03-19 15:45:17 +0530

    Working with data changes - module 2 and 3

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -1,17 +1,21 @@
 import { LightningElement, wire } from 'lwc';
-import getContactList from '@salesforce/apex/ContactController.getContactList';
+import Email_FIELD from '@salesforce/schema/Contact.Email';
+import FirstName_FIELD from '@salesforce/schema/Contact.FirstName';
+import LastName_FIELD from '@salesforce/schema/Contact.LastName';
+import getContacts from '@salesforce/apex/ContactController.getContacts';
+import { reduceErrors } from 'c/ldsUtils';
 
-export default class ContactList extends LightningElement {
-    @wire(getContactList) contacts;
-
-    handleSelect(event) {
-        // 1. Prevent default behavior of anchor tag click which is to navigate to the href url
-        event.preventDefault();
-        // 2. Create a custom event that bubbles. Read about event best practices at http://developer.salesforce.com/docs/component-library/documentation/lwc/lwc.events_best_practices
-        const selectEvent = new CustomEvent('contactselect', {
-            detail: { contactId: event.currentTarget.dataset.contactId }
-        });
-        // 3. Fire the custom event
-        this.dispatchEvent(selectEvent);
+const COLUMNS = [
+    { label: 'First Name', fieldName: FirstName_FIELD.fieldApiName, type: 'text' },
+    { label: 'last Name ', fieldName: LastName_FIELD.fieldApiName, type: 'text' },
+    { label: 'Email', fieldName: Email_FIELD.fieldApiName, type: 'text' }
+];
+export default class AccountList extends LightningElement {
+    columns = COLUMNS;
+    @wire(getContacts)
+    contacts;
+    get errors(){
+        return(this.contacts.error)? reduceErrors(this.contacts.error) : [];
     }
+
 }
